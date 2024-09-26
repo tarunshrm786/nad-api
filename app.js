06a492903ecf97ed033222408242bedf25f0d9ed
Tarun Sharma <tarunshrm768@gmail.com>
2024-09-26 12:18:11 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-// app.js
+// // app.js
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const connectDB = require('./config/db');
+
+// // Load environment variables
+// dotenv.config();
+
+// // Initialize app
+// const app = express();
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Middleware to parse JSON
+// app.use(express.json());
+
+// // Import routes
+// const authRoutes = require('./routes/studentRoutes');
+// const healthRoutes = require('./routes/healthRoutes'); 
+
+// // Use routes
+// app.use('/api/auth', authRoutes); // Student routes (signup, etc.)
+// app.use('/api/health', healthRoutes); // Health check route
+
+// module.exports = app;
+
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 // Load environment variables
@@ -12,12 +39,25 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Define allowed origins
+const allowedOrigins = [
+  'http://localhost:3000/',
+  'https://nad-api-tarunshrm768gmailcoms-projects.vercel.app/',
+];
+
 // Middleware to parse JSON
 app.use(express.json());
 
+// CORS middleware configuration
+app.use(cors({
+  origin: allowedOrigins, // Allow these origins to access your API
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specified HTTP methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
+
 // Import routes
 const authRoutes = require('./routes/studentRoutes');
-const healthRoutes = require('./routes/healthRoutes'); 
+const healthRoutes = require('./routes/healthRoutes');
 
 // Use routes
 app.use('/api/auth', authRoutes); // Student routes (signup, etc.)
